612d444b28c1a14a501cfd4fe341fd93c3451f48
rayanti <rayanticlimatic@gmail.com>
2021-03-06 21:14:54 +0900

    initial commit (copy from private repo)

---
+++ README.md
@@ -0,0 +1,119 @@
+<!-- ABOUT THE PROJECT -->
+## Abstract
+- This project is intended for use in environments where sharing the same equipments among multiple people is required. For example, laboratory, server nodes, room reservations and so on.
+- We would like to relieve the stress of having to negotiate or speculate other peoples reservation.
+- This application enables the user to see other users' reservations in Google Calendar (which should be very familiar to most people).
+## Short description of usage
+- The equiment reservation infos are shown (as events) in Google Calendar. Clients will check the reservations on their PCs or phones, and make a reservation at an avaliable time (by creating events).
+- Settings like display name, what equipments to show in Google Calendar are saved in a Google Sheet.
+## About The Project
+- This project uses a service called [Google Apps Script(GAS)](https://script.google.com/home/start). GAS enables Google services to be connected by scripts. We will use Google Sheets and Google Calendar in this project.
+- The language used in GAS is JavaScript with V8 syntax (V8 is a runtime which is used in Chrome and Node.js).
+- Sheets acts as a database for user information and preferences. Calendar acts as an UI and is used for adding, modifying, reviewing event.
+- There are two types of calendars we will use. The write calendar will be modified by the clients. The read calendar will be modified by the GAS service. When an event is modified on a write calendar, GAS applies the modification on the read calendar.
+
+<!-- GETTING STARTED -->
+### Prerequisites
+You need a Google account.
+
+### Installation (administrator side)
+1. Create a Google account
+2. Create a Google Group
+   1. Change the settings to maximum security in every aspect such as messaging, viewing uses,...
+   2. Enable group joining without admin permission
+3. Create a spreadsheet following the format of `equipmentSettings.xlsx`
+   1. Create 18 calendars in Google Calendar
+   2. For every calendar, set modification priveledge to Google Group users
+   3. write the calendar ids into `Write Calendar id` and `Read Calendar id` rows in sheets
+   4. write the calendar urls into sheet named `URL of Calendar`
+4. Create a GAS Project 
+   1. Copy the contents of `equipmentReservation.js` to the new project's source code
+   2. change {spreadsheetId} to your spreadsheet id.
+5. In the GAS project, run the function CreateTriggers()
+6. Publish the project
+
+<!-- USAGE EXAMPLES -->
+## Usage (client side)
+### Join Google groups using the Google account used for your Google Calendar
+1. access https://groups.google.com/forum/#!forum/{group_name}/join
+2. select "join group"
+3. Fill out the form like the following image. Use a display name that can be understood to the admin.
+4. Subscription is complete. The security settings in the google groups is very strict, therefore you cannot see other members, and cannot post group emails.
+![](pics/googlegroups1.png)
+![](pics/googlegroups2.png)
+
+### Initial settings
+1. Adding you as an user
+   1. Enter your name. Refer to ① in the below picture.
+   2. Link for sheets：https://docs.google.com/spreadsheets/d/{spreadsheet_id}/edit?usp=sharing
+2. Select devices you will use
+   1. Check the devices you would like to use. Refer to ③ in the below picture.
+![](pics/sheets1.png)
+![](pics/sheets2.png)
+
+### Add the user calendars to your calendar application (in your smartphone or on your PC)
+1. Links for Read(read only) calendar and Write(write only) calendar are prepared. (Don't use the link in ➁)
+2. Select the sheet named "URL of Calendar", and click the link like the figure below. Google calendar will open and you can add the calendar.
+![](pics/calendar1.png)
+![](pics/calendar2.png)
+
+### Adding a reservation (Write calendar)
+1. Add an event on Google calendar
+2. Title should be`[Device] [State]`. Refer to ① in the below picture. Refer to `naming conventions` for naming convention of Device and State.
+3. Add the event to `Write [User Name]`. Refer to ① in the below picture.
+
+### naming conventions
+- Devices (__You can change the name of devices in Sheets__)
+-  | Device | Description |
+   | ---- | ---- |
+   | cmp  | Chemical mechanical planarization |
+   | rie  | Reactive ion etching |
+   | cvd  | Chemical vapor deposition |
+   | pvd  | Physical vapor deposition |
+   | euv  | Extreme ultraviolet lithography |
+   | diff | Diffusion |
+   | cleg | Cleaning |
+   | dicg | Dicing |
+   | pack | Packaging |
+- States of equiment
+-  | State | 説明 |
+   | ---- | ---- |
+   | evac | evacuation |
+   | use/[NO ENTRY] | Main operation |
+   | cool | cooling |
+- Users (__You can add users in Sheets__)
+-  | User Name | 名前 |
+   | ---- | ---- |
+   | Tana.Y1 | Tanaka Yuusuke |
+   | Tana.Y2 | Tanabe Yuta |
+   | Tana.S1 | Tanahashi Shion |
+   | Chen.F1 | Chen Fan |
+   | Zhu.Y1  | Zhu Yu |
+
+### Observing reservations (Write calendar)
+1. Observe events on Google Calendar
+2. Event title is `[Name] [Device] [State]`.
+3. If there are too many reservations and it is overwhelming, you can hide the Read calendar in your calendar app and view it only when you need to.
+
+>The implementations details are in [details (Japanese)](https://github.com/rayanti/equipment-reservation/blob/master/details.md).
+
+<!-- ROADMAP -->
+## Roadmap
+See the [open issues](https://github.com/rayanti/equipment-reservation/issues) for a list of proposed features (and known issues).
+
+<!-- CONTRIBUTING -->
+## Contributing
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<!-- LICENSE -->
+## License
+Distributed under the MIT License. See `LICENSE` for more information.
+
+<!-- ACKNOWLEDGEMENTS -->
+## Acknowledgements
+* [Best-README-Template](https://github.com/othneildrew/Best-README-Template)
---
+++ equipmentReservationMerged.js
@@ -0,0 +1,331 @@
+// delete all triggers for this script
+function deleteTriggers(){
+  const triggers = ScriptApp.getProjectTriggers();
+  for (var i = 0; i < triggers.length; i++){
+    trigger = triggers[i];
+    ScriptApp.deleteTrigger(trigger);
+  }
+}
+
+// create triggers
+// only 20 triggers can be made for single script
+// we will use 19 for write calendars, 1 for spreadsheet
+function createTriggers() {
+  const sheet = SpreadsheetApp.openById('{spreadsheetid}').getSheetByName('users');
+  const writeCalendarIds = getWriteCalendarIds(sheet);
+  
+  // create trigger for each of the 19 write calendars
+  // (calls function 'onCalendarEdit' on trigger)
+  Logger.log(writeCalendarIds.length + ' calendar triggers will be created');
+  for (var i = 0; i < writeCalendarIds.length; i++){
+    const writeCalendarId = writeCalendarIds[i];
+    ScriptApp.newTrigger('onCalendarEdit')
+      .forUserCalendar(writeCalendarId)
+      .onEventUpdated()
+      .create(); 
+  }
+  // create 1 Sheets trigger (calls function 'onSheetsEdit' on trigger)
+  ScriptApp.newTrigger('onSheetsEdit')
+      .forSpreadsheet('{spreadsheetid}')
+      .onEdit()
+      .create();
+}
+
+// when calendar gets edited
+function onCalendarEdit(e) {
+  const sheet = SpreadsheetApp.openById('{spreadsheetid}').getSheetByName('users');
+  const calendarId = e.calendarId;
+  const writeCalendarIds = getWriteCalendarIds(sheet);
+  const index = writeCalendarIds.indexOf(calendarId);
+  const fullSync = false;
+  writeEventsToReadCalendar(sheet, calendarId, index, fullSync);
+}
+
+// when sheets gets edited
+function onSheetsEdit(e) {
+  const sheet = e.source.getActiveSheet();
+  const cell = e.source.getActiveRange();
+  const newValue = e.value;
+  const row = cell.getRow();
+  const column = cell.getColumn();
+  const users = getUsers(sheet);
+  const index = row-2;
+  const readUser = users[index];
+  const writeCalendarIds = getWriteCalendarIds(sheet);
+  const calendarId = writeCalendarIds[index]
+  const fullSync = true;
+  
+  // when the checkbox (H2~nm) is edited in sheets on sheet 'users'
+  // update corresponding user's subscribed devices
+  if (sheet.getName() === 'users' && row > 1 && column > 7){ 
+    changeSubscribedDevices(sheet, readUser, index, users);
+  }
+  // when the full name (A2~An) is edited in sheets on sheet 'users'
+  // update all of the corresponding user's event title
+  else if (sheet.getName() === 'users' && row > 1 && column === 1){
+    updateCalendarUserName(sheet, cell, newValue);
+    writeEventsToReadCalendar(sheet, calendarId, index, fullSync);
+  }
+}
+
+// write events to read calendar based on updated events in write calendar
+function writeEventsToReadCalendar(sheet, calendarId, index, fullSync) {
+  const readCalendarIds = getReadCalendars(sheet).readCalendarIds;
+  const enabledDevicesList = getReadCalendars(sheet).enabledDevicesList;
+  const writeCalendarIds = getWriteCalendarIds(sheet);
+  const users = getUsers(sheet);
+  const writeUser = users[index];
+  const events = getEvents(calendarId, fullSync);
+  for (var i = 0; i < readCalendarIds.length; i++){
+    const readUser = users[i];
+    if (readUser != writeUser) { // avoid duplicating event for same user's read and write calendars
+      const readCalendarId = readCalendarIds[i];
+      const enabledDevices = enabledDevicesList[i];
+      writeEvents(events, calendarId, readCalendarId, enabledDevices, writeUser);
+    }
+  }
+}
+
+// update corresponding user's subscribed devices 
+function changeSubscribedDevices(sheet, readUser, index, users){
+  const fullSync = true;
+  const readCalendarIds = getReadCalendars(sheet).readCalendarIds;
+  const enabledDevicesList = getReadCalendars(sheet).enabledDevicesList;
+  const writeCalendarIds = getWriteCalendarIds(sheet);
+  const readCalendarId = readCalendarIds[index];
+  const enabledDevices = enabledDevicesList[index];
+  for (var i = 0; i < writeCalendarIds.length; i++){
+    const writeCalendarId = writeCalendarIds[i];
+    const events = getEvents(writeCalendarId, fullSync);
+    const writeUser = users[i];
+    if (readUser != writeUser) { // avoid duplicating event for same user's read and write calendars
+      writeEvents(events, writeCalendarId, readCalendarId, enabledDevices, writeUser);
+    }
+  }
+}
+  
+// update calendar's User Name based on full name input
+function updateCalendarUserName(sheet, cell, newValue){  
+  setFirstLastNames(sheet, cell, newValue); // get last and first names from full name and set User Name 1
+  setUserNames(sheet); // set User Name 2 using User Name 1
+  setCheckboxes(sheet, cell); // create checkboxes for selecting equipment
+  setCalendars(sheet, cell); // set read calendar and write calendar for created user
+}
+
+// get all the User Names
+function getUsers(sheet) {
+  const users = [];
+  const lastRow = sheet.getLastRow();
+  for (var row = 2; row < lastRow+1; row++) { 
+    const user = sheet.getRange(row, 5).getValue();
+    users.push(user);
+  }
+  return users;
+}
+
+// get all the write calendar's calendarIds
+function getWriteCalendarIds(sheet) {
+  const calendarIds = [];
+  const lastRow = sheet.getLastRow();
+  for (var row = 2; row < lastRow+1; row++) { 
+    calendarId = sheet.getRange(row, 7).getValue();
+    calendarIds.push(calendarId);
+  }
+  return calendarIds;
+}
+
+// get all the read calendar's calendarIds
+function getReadCalendars(sheet) {
+  const calendarIds = [];  
+  const enabledDevicesList = [];
+  const lastRow = sheet.getLastRow();
+  const lastColumn = sheet.getLastColumn();
+  // get calendarId and enabledDevice
+  for (var row = 2; row < lastRow+1; row++) { 
+    // get calendarId and add to calendarIds
+    const calendarId = sheet.getRange(row, 6).getValue();
+    calendarIds.push(calendarId);
+    // get enabledDevice and add to enabledDevices
+    const enabledDevices = [];
+    for (var column = 8; column < lastColumn+1; column++) { 
+      const device = sheet.getRange(1, column).getValue();
+      const checked = sheet.getRange(row, column).isChecked();
+      if (checked === true){
+        enabledDevices.push(device);
+      }
+    }
+    enabledDevicesList.push(enabledDevices);
+  }
+  return {
+    readCalendarIds: calendarIds,
+    enabledDevicesList: enabledDevicesList,
+  };
+}
+
+// get events from the given calendar that have been modified since the last sync.
+// if the sync token is missing or invalid, log all events from up to a month ago (a full sync).
+function getEvents(writeCalendarId, fullSync) {
+  const properties = PropertiesService.getUserProperties();
+  const options = {
+    maxResults: 100
+  };
+  const syncToken = properties.getProperty('syncToken');
+  if (syncToken && !fullSync) {
+    options.syncToken = syncToken;
+  } else {
+    // Sync events up to thirty days in the past.
+    options.timeMin = getRelativeDate(-30, 0).toISOString();
+  }
+
+  // Retrieve events one page at a time.
+  var eventsList;
+  var pageToken;
+  do {
+    try {
+      options.pageToken = pageToken;
+      eventsList = Calendar.Events.list(writeCalendarId, options);
+    } catch (e) {
+      // Check to see if the sync token was invalidated by the server;
+      // if so, perform a full sync instead.
+      if (e.message === 'Sync token is no longer valid, a full sync is required.') {
+        properties.deleteProperty('syncToken');
+        logSyncedEvents(calendarId, true);
+        return;
+      } else {
+        throw new Error(e.message);
+      }
+    }
+    var events = [];
+    if (eventsList.items && eventsList.items.length > 0) {
+      for (var i = 0; i < eventsList.items.length; i++) {
+        const event = eventsList.items[i];
+        if (event.status === 'cancelled') {
+          Logger.log('Event id %s was cancelled.', event.id);
+        } else{
+          events.push(event);
+        }
+      }
+    } else {
+      Logger.log('No events found.');
+    }
+    pageToken = eventsList.nextPageToken;
+  } while (pageToken);
+  properties.setProperty('syncToken', eventsList.nextSyncToken);
+  return events;
+}
+
+// write events from write calendar to read calendar
+function writeEvents(events, writeCalendarId, readCalendarId, enabledDevices, user) {
+  if (events.length > 0) {
+    for (var i = 0; i < events.length; i++) {
+      Utilities.sleep(1000);
+      var event = events[i];
+      const summary = event.summary;
+      const status = summary.split(' '); // split to device and state
+      if (status.length === 1) { // just the device name (state is 'use')
+        var device = status[0];
+        var state = 'use';
+      } else if (status.length === 2 || status.length === 3) { // (User Name) + device + state
+        var device = status[status.length-2];
+        var state = status[status.length-1];
+      }
+      const eid = event.iCalUID;
+      var event = CalendarApp.getCalendarById(writeCalendarId).getEventById(eid);
+      if (enabledDevices.includes(device) === true){ 
+        // if device is enabled in sheets, add to guest subscription
+        // change title from '(User Name) + device + state' to 'User Name + device + state'
+        const summary = user  + ' ' + device + ' ' + state;
+        event.setTitle(summary);
+        event.addGuest(readCalendarId);
+      }
+      else { // if device is enabled in sheets, remove guest subscription
+        event.removeGuest(readCalendarId);
+      }
+    }
+  }
+}
+
+function getRelativeDate(daysOffset, hour) {
+  const date = new Date();
+  date.setDate(date.getDate() + daysOffset);
+  date.setHours(hour);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+  return date;
+}
+
+// get last and first names from full name
+function setFirstLastNames(sheet, cell, newValue){
+  const names = newValue.split(' ', 2); // split to last and first name
+  const lastName = names[1];
+  const firstName = names[0];
+  sheet.getRange(cell.getRow(), 2).setValue(lastName);
+  sheet.getRange(cell.getRow(), 3).setValue(firstName);
+  // set User Name 1 using last and first name
+  // User Name 1 = {Last Name up to 4 letters}.{First Name up to 1 letter}
+  sheet.getRange(cell.getRow(), 4).setValue(lastName.slice(0,4)+'.'+firstName.slice(0,1));
+}
+
+// set User Name 2 using User Name 1
+// User Name 2 = {User Name 1}{unique identifier 1~9}
+//             = {Last Name up to 4 letters}.{First Name up to 1 letter}{unique identifier 1,2,3,...}
+function setUserNames(sheet){
+  const lastRow = sheet.getLastRow();
+  // update User Name 2 for row0 = 2~lastRow
+  for (var row0 = 2; row0 < lastRow+1; row0++) {
+    const value0 = sheet.getRange(row0, 4).getValue();
+    var count = 0;
+    // check the duplicate count of value0 for row1 = 2~row0
+    for (var row1 = 2; row1 < row0+1; row1++) {
+      const value1 = sheet.getRange(row1, 4).getValue();
+      if (value0 === value1){
+        count += 1;
+      }
+    }
+    // use count as unique identifier (1,2,3,...)
+    sheet.getRange(row0, 5).setValue(value0+count); 
+  }
+}
+
+// create checkboxes for selecting which equipment to show in the calendar
+function setCheckboxes(sheet, cell) {
+  const lastColumn = sheet.getLastColumn();
+  if (sheet.getRange(cell.getRow(), 8).isChecked() == null){ // if cell is not a checkbox
+    // create checkboxes
+    for (var column = 8; column < lastColumn+1; column++) { 
+      sheet.getRange(cell.getRow(), column).insertCheckboxes();  
+    }
+  }
+}
+
+// set read calendar and write calendar for created user
+function setCalendars(sheet, cell) {
+  const userName = sheet.getRange(cell.getRow(), 5).getValue();
+  const readCalendarId = sheet.getRange(cell.getRow(), 6).getValue();
+  const writeCalendarId = sheet.getRange(cell.getRow(), 7).getValue();
+  changeCalendarName(readCalendarId, userName, 'Read');
+  changeCalendarName(writeCalendarId, userName, 'Write');
+}
+
+// update calendar name and description
+function changeCalendarName(calendarId, userName, readOrWrite) {
+  // calendar name (summary) and description changes for read and write calendar
+  if (readOrWrite === 'Read') { 
+    var summary = 'Read' + ' ' + userName;
+    var description = '装置の予約状況\n' +
+      'schedule for selected devices';  
+  } else if (readOrWrite === 'Write') { 
+    var summary = 'Write' + ' ' + userName;
+    var description = '装置を予約する\n' +
+      'Reserve devices\n' +
+      'Formatting: [Device] [State]\n' +
+      'Devices: rie, nrie(new RIE), cvd, ncvd(new CVD), pvd, fts\n' +
+      'States: evac(evacuation), use(or no entry), cool(cooldown), o2(RIE O2 ashing)\n';  
+  } else {
+    Logger.log('readOrWrite has to be \'Read\' or \'Write\'');
+  };
+  const calendar = CalendarApp.getCalendarById(calendarId);
+  calendar.setName(summary);
+  calendar.setDescription(description);
+}
